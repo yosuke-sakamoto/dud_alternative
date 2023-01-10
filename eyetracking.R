ab4365a65771ac0ab8656b7338e9861ead6e158e
kiyomiyoshi <miyoshi80@gmail.com>
2023-01-10 16:22:20 +0900

    commit

---
+++ eyetracking.R
@@ -0,0 +1,126 @@
+#+ message = F
+library(tidyverse)
+library(data.table)
+library(ggh4x)
+library(lme4)
+library(car)
+library(ggeffects)
+library(doParallel)
+
+cores <- getOption("mc.cores", detectCores()) # for parallel computation
+cl <- makeCluster(cores)
+registerDoParallel(cl)
+
+
+#'# data loading
+files <- dir("exp1_data_eyetracking_individual_saccades", ".*csv$", full.names = TRUE) 
+dat <- c()
+
+for (i in 1:length(files)) {
+    d <- fread(files[i], header = TRUE)
+    d$id <- i  # numeric variable for subject id
+    dat <- rbind(dat, d)
+}
+
+dat <- subset(dat, dat$event == "fixation")
+dat <- mutate(dat, target = apply(dat, 1, function(x){names(d)[13 + which.max(x[14:16])]}))
+dat <- mutate(dat, dud = apply(dat, 1, function(x){names(d)[13 + which.min(x[14:16])]}))
+dat$Condition <- as.factor(dat$Condition)
+dat$target <- fct_recode(dat$target, up = "UVal", left = "LVal", right = "RVal")
+dat$dud <- fct_recode(dat$dud, up = "UVal", left = "LVal", right = "RVal")
+dat <- mutate(dat, fix = ifelse(item == target, "target", 
+                         ifelse(item == dud, "dud",
+                         ifelse(item == "noFix", "noFix", "distractor"))))
+
+
+#'# subject-wise fixation plot
+plot1 <- foreach(i = 1:length(files), .packages = c("tidyverse", "ggh4x")) %dopar% {
+    subset(dat, dat$id == i) %>%
+    ggplot() + geom_point(aes(x = x, y = y, size = dur, color = Condition), alpha = 0.3) + 
+        facet_nested(. ~ target + dud) + ggtitle(i) -> p
+    print(p)
+}
+plot1
+
+
+#'# Position-based fixation frequency
+dat %>%
+    group_by(Condition, target, dud, item, id) %>%
+    summarise(n = n()) -> freq
+
+plot2 <- foreach(i = unique(freq$Condition), .packages = c("tidyverse", "ggh4x")) %dopar% {
+    p <- ggplot(subset(freq, freq$Condition == i)) + geom_violin(aes(x = item, y = n, color = item)) + geom_point(aes(x = item, y = n)) + 
+        facet_nested(. ~ target + dud) + 
+        scale_x_discrete(limits = c("up", "left", "right", "noFix")) +
+        scale_color_discrete(limits = c("up", "left", "right", "noFix")) + ggtitle(i)
+    print(p)
+}
+plot2
+
+
+#'# Stimulus-based fixation frequency
+dat %>%
+    group_by(Condition, fix, id) %>%
+    summarise(n = n()) -> fixation
+
+fixation$fix <- as.factor(fixation$fix)
+fixation$fix <- factor(fixation$fix, levels = c("target", "distractor", "dud", "noFix"))
+
+ggplot(fixation, aes(x = fix, y = n, color = Condition)) + geom_violin() + 
+    geom_point(position = position_dodge(width = .9)) +
+    stat_summary(fun.y = "mean", geom = "crossbar", position = position_dodge(width = .9)) 
+
+f1 <- lmer(n ~ fix * Condition + (1 + fix + Condition | id), data = fixation)
+summary(f1)
+Anova(f1)
+plot(ggpredict(f1, terms = c("fix", "Condition"), type = "fe"))
+
+
+#'# Stimulus-based fixation frequency
+fixation_prop1 <- c()
+
+for (i in unique(fixation$id)) {
+    for (j in unique(fixation$Condition)) {
+        d <- subset(fixation,  fixation$id == i & fixation$Condition == j)
+        s <- sum(d$n)
+        fixation_prop1 <- rbind(fixation_prop1, mutate(d, prop = n/s))
+    }
+}
+
+ggplot(fixation_prop1, aes(x = fix, y = prop, color = Condition)) + geom_violin() + 
+    geom_point(position = position_dodge(width = .9)) +
+    stat_summary(fun.y = "mean", geom = "crossbar", position = position_dodge(width = .9)) 
+
+
+#'# Stimulus-based fixation frequency (noFix excluded)
+fixation2 <- subset(fixation, fixation$fix != "noFix")
+fixation_prop2 <- c()
+
+for (i in unique(fixation2$id)) {
+    for (j in unique(fixation2$Condition)) {
+        d <- subset(fixation2,  fixation2$id == i & fixation2$Condition == j)
+        s <- sum(d$n)
+        fixation_prop2 <- rbind(fixation_prop2, mutate(d, prop = n/s))
+    }
+}
+
+ggplot(fixation_prop2, aes(x = fix, y = prop, color = Condition)) + geom_violin() + 
+    geom_point(position = position_dodge(width = .9)) +
+    stat_summary(fun.y = "mean", geom = "crossbar", position = position_dodge(width = .9)) 
+
+
+#'# Stimulus-based fixation frequency (noFix excluded)
+fixation3 <- subset(fixation, fixation$fix == "target" | fixation$fix == "distractor")
+fixation_prop3 <- c()
+
+for (i in unique(fixation3$id)) {
+    for (j in unique(fixation3$Condition)) {
+        d <- subset(fixation3,  fixation3$id == i & fixation3$Condition == j)
+        s <- sum(d$n)
+        fixation_prop3 <- rbind(fixation_prop3, mutate(d, prop = n/s))
+    }
+}
+
+ggplot(fixation_prop3, aes(x = fix, y = prop, color = Condition)) + geom_violin() + 
+    geom_point(position = position_dodge(width = .9)) +
+    stat_summary(fun.y = "mean", geom = "crossbar", position = position_dodge(width = .9)) 
