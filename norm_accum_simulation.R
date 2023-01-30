a11f579bcb19fed039e3646adc496ba2d4e9fa02
kiyomiyoshi <miyoshi80@gmail.com>
2023-01-30 20:15:32 +0900

    commit

--- norm_accum_simulation.R
+++ norm_accum_simulation.R
@@ -1,4 +1,4 @@
-# import library
+# import library dceにより重きを置く
 library(doParallel)
 library(magrittr)
 library(dplyr)
@@ -15,7 +15,7 @@ sigma <- 0.06
 n_dt  <- 250
 w     <- -0.0022
 
-Trial <- 10000
+Trial <- 2880
 th <- 1.2
 Sample <- 3000
 condition <- c(0, 27, 45, 63, 76, 86)
@@ -56,7 +56,7 @@ for (j in condition) {
     d$rt   <- d$rt + n_dt
     d$rt[d$rt > 3000] <- NA
     d <- na.omit(d)
-    d$rt <- d$rt/1000 #####
+    d$rt <- d$rt/1000
     d$condition <- j
     simdat <- rbind(simdat, d)
     
@@ -109,3 +109,69 @@ simdat %>%
     summarise(mean_post_delta = mean(post_delta)) %>%
     ggplot() + geom_line(aes(x = condition, y = mean_post_delta, color = factor(choice))) +
     scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86))
+
+
+### fitting confidence data
+df <- read.csv("exp1_data_behavior.csv", header = TRUE)
+df <- na.omit(df)
+df$chosenItem <- ifelse(df$chosenItem == "target", 1, 
+                        ifelse(df$chosenItem == "distractor", 2, 3))
+df01 <- filter(df, subj == "sub01" & chosenItem != 3)
+
+fit_conf <- function(simdat, empdat) {
+    guess <- c(0.7, 1, 1.5, 2)
+    
+    params <- list("m1_correct" =   filter(simdat, choice == 1)$m1,
+                   "m2_correct" =   filter(simdat, choice == 1)$m2,
+                   "m1_incorrect" = filter(simdat, choice == 2)$m1,
+                   "m2_incorrect" = filter(simdat, choice == 2)$m2,
+                   "conf_correct" = filter(empdat, chosenItem == 1)$conf,
+                   "conf_incorrect" = filter(empdat, chosenItem ==2)$conf)
+    
+    fit <- suppressWarnings(optim(par = guess, fn = fit_conf_ks, gr = NULL, method = "L-BFGS-B", parameters = params,
+                                  lower = c(0, 0, 0, 0),
+                                  upper = c(1, 10, 10, 10),
+                                  control = list("maxit" = 100000, "parscale" = c(0.001, 0.001, 0.001, 0.001))))
+    
+    est <- data.frame(weight = fit$par[1], c1 = fit$par[2], c2 = fit$par[3], c3 = fit$par[4], ks = fit$value)
+    return(est)
+}
+
+
+fit_conf_ks <- function(x, parameters) {
+    w  <- x[1]
+    c1 <- x[2] 
+    c2 <- x[3]
+    c3 <- x[4]
+    
+    cv_incorrect <- parameters$m1_incorrect - w * parameters$m2_incorrect
+    cv_correct <- parameters$m1_correct - w * parameters$m2_correct
+    simconf_correct <- c(rep(1, sum(cv_incorrect < c1)), rep(2, sum(cv_incorrect >= c1 & cv_incorrect< c2)), 
+                         rep(3, sum(cv_incorrect >= c2 & cv_incorrect < c3)), rep(4, sum(cv_incorrect >= c3)))
+    simconf_incorrect <-  c(rep(1, sum(cv_correct < c1)), rep(2, sum(cv_correct >= c1 & cv_correct< c2)), 
+                            rep(3, sum(cv_correct >= c2 & cv_correct < c3)), rep(4, sum(cv_correct >= c3)))
+    simconf <- c(simconf_correct, -1 * simconf_incorrect)
+    empconf <- c(parameters$conf_correct, -1 * parameters$conf_incorrect)
+    
+    ks <- suppressWarnings(ks.test(simconf, empconf)$statistic) # Kolmogorov-Smirnov test
+    if (is.nan(ks)) {
+        ks <- Inf
+    }
+    return(ks)
+}
+
+f <- fit_conf(simdat = simdat, empdat = df01)
+
+
+simdat <- mutate(simdat, simconf = m1 - as.numeric(f[1]) * m2)
+simdat %>%
+    group_by(choice, condition) %>%
+    summarise(mean_conf = mean(simconf)) %>%
+    ggplot() + geom_line(aes(x = condition, y = mean_conf, color = factor(choice))) +
+    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86))
+
+
+for (i in unique(simdat$condition)) {
+    df <- subset(simdat, simdat$condition == i & choice != 3)
+    df <- mutate(df, cv = m1 - w * m2)
+}
