8428e6ed74836d2286f59dcd7b96588f89ed45d9
kiyomiyoshi <miyoshi80@gmail.com>
2023-02-01 15:46:32 +0900

    commit

--- norm_accum_simulation.R
+++ norm_accum_simulation.R
@@ -1,4 +1,4 @@
-# import ２AFCは別ルートで統合
+# import
 library(doParallel)
 library(magrittr)
 library(dplyr)
@@ -16,18 +16,18 @@ sigma <- 0.06
 n_dt  <- 250
 w     <- -0.0022
 
-Trial <- 3000
+Trial <- 10000
 th <- 1.2
+threshold <- rnorm(Trial, 1.2, 0.1) # jitter for threshold
 Sample <- 3000
-condition <- c(27, 45, 63, 76, 86)
+condition <- c(0, 27, 45, 63, 76, 86)
 simdat <- c()
 
 tic()
-threshold <- rnorm(Trial, 1.2, 0.1) # jitter for threshold
 for (j in condition) {
     
     d <- foreach(i = 1:Trial, .combine = "rbind", .packages = c("magrittr", "dplyr")) %dopar% {
-        #th <- threshold[i] # jitter for threshold
+        # th <- threshold[i] # jitter for threshold
         Stimulus <- c(100, 90, j)
         Stimulus <- Stimulus / (1 + w * sum(Stimulus))
         Stimulus <- Stimulus * dr
@@ -39,31 +39,30 @@ for (j in condition) {
         accum[-1, 1] <- accum[-1, 1] + rnorm(Sample, 0, sigma)
         accum[-1, 2] <- accum[-1, 2] + rnorm(Sample, 0, sigma)
         accum[-1, 3] <- accum[-1, 3] + rnorm(Sample, 0, sigma)
+        accum <- accum[, 1:nAlt]
         accum <- cumsum(accum)[-1, ]
-       # accum <- accum[, 1:nAlt]
-        accum %>% mutate(accum, first =  max.col(accum), third = max.col(-accum)) %>%
-            rowwise() %>% mutate(second = setdiff(c(1, 2, 3), c(first, third))) %>%
-            rowwise() %>% mutate(delta = pmax(a1, a2, a3) - sort(c(a1, a2, a3), decreasing = TRUE)[2]) -> accum
+        accum <- mutate(accum, delta = apply(accum, 1, max) - apply(accum, 1, function(x){return(max(x[-which.max(x)]))}))
         
-        simrt <- which(accum$delta > th)[1]
+        simrt  <- which(accum$delta > th)[1]
         choice <- max.col(accum[, 1:nAlt])[simrt]
+        third  <- max.col(-accum[, 1:nAlt])[simrt]
+        second <- ifelse(nAlt == 3, setdiff(c(1, 2, 3), c(choice, third)), third)
         
-        pre <- ifelse(simrt < 300, simrt - 1, 300)
+        pre <- ifelse(simrt <= 300, simrt - 1, 300)
         post <- 300
         
         as.numeric(c(simrt, choice,
-          accum[simrt, accum$first[simrt]], accum[simrt, accum$second[simrt]], accum[simrt, accum$third[simrt]],
-          accum[simrt - pre, accum$first[simrt]], accum[simrt - pre, accum$second[simrt]], accum[simrt - pre, accum$third[simrt]],                                      
-          accum[simrt + post, accum$first[simrt]], accum[simrt + post, accum$second[simrt]], accum[simrt + post, accum$third[simrt]]))                                                                   
-          
+                     accum[simrt, choice],        accum[simrt, second],        accum[simrt, third],
+                     accum[simrt - pre, choice],  accum[simrt - pre, second],  accum[simrt - pre, third],                                      
+                     accum[simrt + post, choice], accum[simrt + post, second], accum[simrt + post, third]))                                                                   
+        
     }
     
     d <- as.data.frame(d)
-    colnames(d) <- c("rt", "choice", "a1", "a2", "a3", "a1_pre", "a2_pre", "a3_pre", "a1_post", "a2_post", "a3_post")
+    colnames(d) <- c("rt", "choice", "m1", "m2", "m3", "m1_pre", "m2_pre", "m3_pre", "m1_post", "m2_post", "m3_post")
     d$rt   <- d$rt + n_dt
-    d$rt[d$rt > 3000] <- NA
-    d <- na.omit(d)
-    d$rt <- d$rt/1000
+    #d$rt[d$rt > 3000] <- NA
+    #d <- na.omit(d)
     d$condition <- j
     simdat <- rbind(simdat, d)
     
@@ -71,7 +70,7 @@ for (j in condition) {
 toc()
 
 # model visualization
-simdat %>% mutate(delta = a1 - a2, delta_pre = a1_pre - a2_pre, delta_post = a1_post - a2_post) -> simdat
+simdat %>% mutate(delta = m1 - m2, delta_pre = m1_pre - m2_pre, delta_post = m1_post - m2_post) -> simdat
 simdat %>%
     complete(choice, condition) %>%
     ggplot() + geom_histogram(aes(x = rt, fill = factor(choice))) + facet_wrap(. ~ factor(condition) + factor(choice), nrow = 6) -> p1
@@ -97,31 +96,25 @@ simdat %>%
     scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) -> p3
 p3
 
+### at choice
 simdat %>%
     group_by(choice, condition) %>%
-    summarise(mean_a1 = mean(a1)) %>%
-    ggplot() + geom_line(aes(x = condition, y = mean_a1, color = factor(choice))) +
+    summarise(mean_m1 = mean(m1)) %>%
+    ggplot() + geom_line(aes(x = condition, y = mean_m1, color = factor(choice))) +
     scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) + ylim(0, 8) -> p4
 p4
 
 simdat %>%
     group_by(choice, condition) %>%
-    ggplot(aes(x = condition, y = a1, color = factor(choice))) + geom_point() +
-    stat_summary(fun.y = "mean", geom = "line") +
-    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) + ylim(0, 8) -> p4
-p4
-
-simdat %>%
-    group_by(choice, condition) %>%
-    summarise(mean_a2 = mean(a2)) %>%
-    ggplot() + geom_line(aes(x = condition, y = mean_a2, color = factor(choice))) +
+    summarise(mean_m2 = mean(m2)) %>%
+    ggplot() + geom_line(aes(x = condition, y = mean_m2, color = factor(choice))) +
     scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) + ylim(0, 8) -> p5
 p5
 
 simdat %>%
     group_by(choice, condition) %>%
-    summarise(mean_a3 = mean(a3)) %>%
-    ggplot() + geom_line(aes(x = condition, y = mean_a3, color = factor(choice))) +
+    summarise(mean_m3 = mean(m3)) %>%
+    ggplot() + geom_line(aes(x = condition, y = mean_m3, color = factor(choice))) +
     scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) + ylim(0, 8) -> p6
 p6
 
@@ -129,22 +122,63 @@ simdat %>%
     group_by(choice, condition) %>%
     summarise(mean_delta = mean(delta)) %>%
     ggplot() + geom_line(aes(x = condition, y = mean_delta, color = factor(choice))) +
-    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) -> p6
-p6
+    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) -> p7
+p7
+
+### before choice
+simdat %>%
+    group_by(choice, condition) %>%
+    summarise(mean_m1_pre = mean(m1_pre)) %>%
+    ggplot() + geom_line(aes(x = condition, y = mean_m1_pre, color = factor(choice))) +
+    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) + ylim(0, 8) -> p8
+p8
+
+simdat %>%
+    group_by(choice, condition) %>%
+    summarise(mean_m2_pre = mean(m2_pre)) %>%
+    ggplot() + geom_line(aes(x = condition, y = mean_m2_pre, color = factor(choice))) +
+    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) + ylim(0, 8) -> p9
+p9
+
+simdat %>%
+    group_by(choice, condition) %>%
+    summarise(mean_m3_pre = mean(m3_pre)) %>%
+    ggplot() + geom_line(aes(x = condition, y = mean_m3_pre, color = factor(choice))) +
+    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) + ylim(0, 8) -> p10
+p10
 
 simdat %>%
     group_by(choice, condition) %>%
     summarise(mean_delta_pre = mean(delta_pre)) %>%
     ggplot() + geom_line(aes(x = condition, y = mean_delta_pre, color = factor(choice))) +
-    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) -> p7
-p7
+    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) -> p11
+p11
+
+### post choice
+simdat %>%
+    group_by(choice, condition) %>%
+    summarise(mean_m1_post = mean(m1_post)) %>%
+    ggplot() + geom_line(aes(x = condition, y = mean_m1_post, color = factor(choice))) +
+    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) -> p12
+p12
+
+simdat %>%
+    group_by(choice, condition) %>%
+    summarise(mean_m2_post = mean(m2_post)) %>%
+    ggplot() + geom_line(aes(x = condition, y = mean_m2_post, color = factor(choice))) +
+    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) -> p13
+p13
+
+simdat %>%
+    group_by(choice, condition) %>%
+    summarise(mean_m3_post = mean(m3_post)) %>%
+    ggplot() + geom_line(aes(x = condition, y = mean_m3_post, color = factor(choice))) +
+    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) -> p14
+p14
 
 simdat %>%
     group_by(choice, condition) %>%
     summarise(mean_delta_post = mean(delta_post)) %>%
     ggplot() + geom_line(aes(x = condition, y = mean_delta_post, color = factor(choice))) +
-    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) -> p8
-p8 #平均だけではわからないのでgrid searchでモデリングする
-
-plot(simdat$a1, simdat$a2_pre) # 3つの関係を図示
-plot(simdat$a1, simdat$a2_post)
+    scale_x_continuous(breaks = c(0, 27, 45, 63, 76, 86)) -> p15
+p15
